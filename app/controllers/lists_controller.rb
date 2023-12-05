df6f3a4c11a602cf7e33244c8729755c8a58c218
kimiko ando <kimiko.ando13@icloud.com>
2023-12-05 11:40:07 +0900

    updated the index page but removed the cards

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -18,14 +20,20 @@ class ListsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
-    params.require(:list).permit(
-      :name, :photo
-    )
+    params.require(:list).permit(:name, :photo)
   end
 end
